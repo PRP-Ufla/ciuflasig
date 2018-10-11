827b086b044273c4f828b21d6874cc8c46ddda8e
PRP-Ufla <sistemasprpufla@gmail.com>
2018-10-11 11:56:42 -0300

    Impedir que um avaliador selecione mais de um proposta por sessão

--- avaliadores/php/avaliarResumo.php
+++ avaliadores/php/avaliarResumo.php
@@ -20,19 +20,41 @@
 	$sql = 'SELECT * FROM vaga WHERE id = '.$vagaId.' AND disponiveis > 0;';
 	$res = $db1->consulta($sql);
 	
-	$sql = 'SELECT * FROM avaliacoes WHERE avaliador_id = '.$avaliadorId.' AND vaga_id = '.$vagaId.';';
-	$sess = $db1 -> consulta($sql);
-	
+	//Consulta para verificar se o avaliador está avaliando a mesma vaga duas vezes
+	//se tornou desnecessário devido a nova consulta logo a baixo
+	// $sql = 'SELECT * FROM avaliacoes WHERE avaliador_id = '.$avaliadorId.' AND vaga_id = '.$vagaId.';';
+	// $sess = $db1 -> consulta($sql);
+
+	//Verifica se o avaliador está escolhendo uma avaliação da mesma sessão e do mesmo evento
+	$sql =	'SELECT *
+					FROM vaga 
+					JOIN avaliacoes 
+					ON (vaga.id = avaliacoes.vaga_id)
+					JOIN 
+					(SELECT vaga.sessao_id AS sessao_id, vaga.evento_id AS evento_id 
+							FROM vaga
+							WHERE vaga.id = '.$vagaId.') AS vagaAtual
+							ON (vaga.sessao_id = vagaAtual.sessao_id
+							AND vaga.evento_id = vagaATual.evento_id)
+					WHERE avaliacoes.avaliador_id = '.$avaliadorId.';';
+
+	$sessao = $db1 -> consulta($sql);
+
 	if (count($res) == 0){
 		echo "0";
 	}
 	
-	else if (count($sess) > 0) {
+	//Ler a linha 23
+	// else if (count($sess) > 0) {
+	// 	echo "1";
+	// }
+
+	else if (count($sessao) > 0){
 		echo "1";
 	}
-	
+
 	else {
-		$decrementarDisponibilidadeDeVagaSQL = 'UPDATE vaga SET disponiveis = disponiveis-1 WHERE id = '.$vagaId.';';
+		$decrementarDisponibilidadeDeVagaSQL = 'UPDATE vaga SET disponiveis = disponiveis - 1 WHERE id = '.$vagaId.';';
 
 		$avaliacoesSQL = 'INSERT INTO avaliacoes (avaliador_id, vaga_id) VALUES ("'.$avaliadorId.'", "'.$vagaId.'");';
 
